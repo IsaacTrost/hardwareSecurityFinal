5cc424549f15bf335041d58e0e45343f8315ddff
IsaacTrost <wisaactrost@gmail.com>
2025-05-13 22:40:37 -0400

    down to 200

--- Pt3/testing/test_speed.py
+++ Pt3/testing/test_speed.py
@@ -21,8 +21,8 @@ args, _ = parser.parse_known_args()
 API_BASE_URL = f"https://{args.ip}/records"
 INITIAL_RECORDS_TO_LOAD = 100000
 WRITE_PERCENTAGE = 0.10 # 10% writes
-MAX_CONCURRENT_REQUESTS = 500 # Adjust based on your server's capacity and client machine
-INITIAL_WRITE_CONCURRENCY = 500  # Lower concurrency for initial writes
+MAX_CONCURRENT_REQUESTS = 200 # Adjust based on your server's capacity and client machine
+INITIAL_WRITE_CONCURRENCY = 200  # Lower concurrency for initial writes
 
 # --- Data Generation ---
 def generate_random_string(length=10):
